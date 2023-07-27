53688a0e84b91f55ad6d947a79a5f2f07c88e642
Adam Chen <adamchen2301@gmail.com>
2023-07-28 01:27:13 +1000

    Added random_state arguments

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,35 +1,33 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn import metrics
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.model_selection import train_test_split
 from process_data import getBinaryClassData, Normalize
 from evaluate_model import EvaluateModel
 from imblearn.over_sampling import SMOTE
 
 X,y = getBinaryClassData()
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, stratify=y, shuffle=True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, stratify=y, shuffle=True, random_state=1)
 X_train, X_test = Normalize(X_train, X_test)
 X_train = np.nan_to_num(X_train)
 X_test = np.nan_to_num(X_test)
+model = LogisticRegression(random_state=1)
 
 # Class Weights
 grid = {
-    'C': [0.5,1.0,1.5],
+    'C': [0.5,0.75,1.0,1.25,1.5],
     'fit_intercept': [True,False],
     'class_weight': ['balanced', None]
 }
 
-EvaluateModel(X_train, y_train, X_test, y_test, LogisticRegression(), grid)
+EvaluateModel(X_train, y_train, X_test, y_test, model, grid)
 
 # Oversampling
 oversample = SMOTE()
 X_train, y_train = oversample.fit_resample(X_train, y_train)
 
 grid = {
-    'C': [0.5,1.0,1.5],
+    'C': [0.5,0.75,1.0,1.25,1.5],
     'fit_intercept': [True,False]
 }
 
-EvaluateModel(X_train, y_train, X_test, y_test, LogisticRegression(), grid)
+EvaluateModel(X_train, y_train, X_test, y_test, model, grid)
