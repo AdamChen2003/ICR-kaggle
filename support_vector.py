ad98f408d52c159c4abcaed14ad83a4acbe786a4
Adam Chen <adamchen2301@gmail.com>
2023-07-31 04:06:05 +1000

    Implemented support vector classifier

---
+++ support_vector.py
@@ -0,0 +1,38 @@
+import numpy as np
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from process_data import getBinaryClassData, Normalize, pca
+from evaluate_model import EvaluateModel
+from imblearn.over_sampling import SMOTE
+
+X,y = getBinaryClassData()
+# X = pca(X)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, stratify=y, shuffle=True, random_state=1)
+X_train, X_test = Normalize(X_train, X_test)
+X_train = np.nan_to_num(X_train)
+X_test = np.nan_to_num(X_test)
+model = SVC()
+
+# No sampling
+grid = {
+    'C': [0.1, 1, 10, 100, 1000],
+    'gamma': [1, 0.1, 0.01, 0.001, 0.0001],
+    'kernel': ['rbf','linear','sigmoid','poly'],
+    'degree': [2,3,4,5],
+    'class_weight': ['balanced', None]
+}
+
+EvaluateModel(X_train, y_train, X_test, y_test, model, grid)
+
+# Oversampling
+oversample = SMOTE()
+X_train, y_train = oversample.fit_resample(X_train, y_train)
+
+grid = {
+    'C': [0.1, 1, 10, 100, 1000],
+    'gamma': [1, 0.1, 0.01, 0.001, 0.0001],
+    'kernel': ['rbf','linear','sigmoid','poly'],
+    'degree': [2,3,4,5]
+}
+
+EvaluateModel(X_train, y_train, X_test, y_test, model, grid)
