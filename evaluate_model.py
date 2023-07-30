0aa4d4f7b5da6fae7bd4ad68d8f72b46efbe068f
Adam Chen <adamchen2301@gmail.com>
2023-07-31 03:59:42 +1000

    Included f2 metric

--- evaluate_model.py
+++ evaluate_model.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.metrics import confusion_matrix, f1_score
+from sklearn.metrics import confusion_matrix, f1_score, fbeta_score
 from sklearn.model_selection import GridSearchCV
 
 def EvaluateModel(X_train, y_train, X_test, y_test, model, grid):
@@ -11,6 +11,7 @@ def EvaluateModel(X_train, y_train, X_test, y_test, model, grid):
     print(f"best score on training set: {cv.best_score_}")
     print(f"best model accuracy on test set: {score}")
     print(f"f1 score: {f1_score(cv.best_estimator_.predict(X_test), y_test.ravel())}")
+    print(f"f2 score: {fbeta_score(cv.best_estimator_.predict(X_test), y_test.ravel())}")
 
     plt.figure(figsize=(9,9))
     cm = confusion_matrix(y_test, cv.best_estimator_.predict(X_test))
