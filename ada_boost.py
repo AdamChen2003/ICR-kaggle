01364ff8a2f76a3fe5dc10361161a21f22714d17
Adam Chen <adamchen2301@gmail.com>
2023-07-31 02:58:23 +1000

    Reimplemented Adaboost

--- ada_boost.py
+++ ada_boost.py
@@ -1,62 +1,39 @@
+import numpy as np
 from sklearn.ensemble import AdaBoostClassifier
+from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-from process_data import getBinaryClassData, getBinaryClassWeights, getMultiClassData, getMultiClassWeights
+from process_data import getBinaryClassData, Normalize
+from evaluate_model import EvaluateModel
+from imblearn.over_sampling import SMOTE
 
-X, y = getBinaryClassData()
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, stratify=y, shuffle=True)
+X,y = getBinaryClassData()
+# X = pca(X)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, stratify=y, shuffle=True, random_state=1)
 X_train, X_test = Normalize(X_train, X_test)
 X_train = np.nan_to_num(X_train)
 X_test = np.nan_to_num(X_test)
+model = AdaBoostClassifier(random_state=1)
 
-# model = AdaBoostClassifier(estimator=DecisionTreeClassifier(max_depth=1, criterion='entropy',class_weight=getBinaryClassWeights()),n_estimators=1000)
-# EvaluateModel(X, y, model)
+# No sampling
+base_models = []
 
-# model = AdaBoostClassifier(estimator=DecisionTreeClassifier(max_depth=1, criterion='entropy',class_weight=getMultiClassWeights()),n_estimators=1000)
-# EvaluateModel(X, y, model, True)
+for i in [1,2,3]:
+    base_models.append(DecisionTreeClassifier(max_depth=1, criterion='entropy'))
 
-from statistics import stdev
-import numpy as np
-from sklearn.model_selection import StratifiedKFold, train_test_split
-from sklearn.metrics import f1_score
-from process_data import Normalize
+grid = {
+    'estimator': base_models,
+    'n_estimators': [200,500,1000]
+}
 
-# multiclass = True
-# X,y = getMultiClassData()
-# skf = StratifiedKFold(n_splits=5,shuffle=True)
-# scores = []
-# f1_scores = []
-# for train_index, test_index in skf.split(X,y):
-#     X_train, X_test = X[train_index], X[test_index]
-#     y_train, y_test = y[train_index], y[test_index]
-#     X_train, X_test = Normalize(X_train, X_test)
-#     X_train = np.nan_to_num(X_train)
-#     X_test = np.nan_to_num(X_test)
-#     model = AdaBoostClassifier(estimator=DecisionTreeClassifier(max_depth=1, criterion='entropy',class_weight=getMultiClassWeights(y_train)),n_estimators=1000)
-#     model.fit(X_train, y_train.ravel())
-#     scores.append(model.score(X_test, y_test.ravel()))
-#     if multiclass:
-#         pred = model.predict(X_test)
-#         f1_scores.append(f1_score(np.where(y_test > 0, 1, y_test), np.where(pred > 0, 1, pred), average='micro'))
-#     else:
-#         f1_scores.append(f1_score(y_test, model.predict(X_test), average='micro'))
+EvaluateModel(X_train, y_train, X_test, y_test, model, grid)
 
-# print(f'Maximum Accuracy Score: {max(scores)}')
-# print(f'Minimum Accuracy Score: {min(scores)}')
-# print(f'Mean Accuracy Score: {np.mean(scores)}')
-# print(f'Accuracy Standard Deviation: {stdev(scores)}')
-# print()
-# print(f'Maximum F1 Score: {max(f1_scores)}')
-# print(f'Minimum F1 Score: {min(f1_scores)}')
-# print(f'Mean F1 Score: {np.mean(f1_scores)}')
-# print(f'F1 Standard Deviation: {stdev(f1_scores)}')
+# Oversampling
+oversample = SMOTE()
+X_train, y_train = oversample.fit_resample(X_train, y_train)
 
-# base_models = []
+grid = {
+    'estimator': base_models,
+    'n_estimators': [200,500,1000]
+}
 
-# for i in [1,3,6,8]:
-#     base_models.append(DecisionTreeClassifier(max_depth=1, criterion='entropy',class_weight=getMultiClassWeights(y_train)))
-
-# grid = {
-#     'estimator': [],
-#     'criterion': ['gini','entropy'],
-#     'n_estimators': [200,500,1000]
-# }
+EvaluateModel(X_train, y_train, X_test, y_test, model, grid)
