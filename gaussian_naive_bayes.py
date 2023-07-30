44a50fd7a3545b6ec102fe7f31f9f3aa188010ab
Adam Chen <adamchen2301@gmail.com>
2023-07-31 02:57:50 +1000

    Added a non-sampling implementation'

--- gaussian_naive_bayes.py
+++ gaussian_naive_bayes.py
@@ -6,12 +6,15 @@ from evaluate_model import EvaluateModel
 from imblearn.over_sampling import SMOTE
 
 X,y = getBinaryClassData()
-X = pca(X)
+# X = pca(X)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, stratify=y, shuffle=True, random_state=1)
 X_train, X_test = Normalize(X_train, X_test)
 X_train = np.nan_to_num(X_train)
 X_test = np.nan_to_num(X_test)
 model = GaussianNB()
+
+# No sampling
+EvaluateModel(X_train, y_train, X_test, y_test, model, {})
 
 # Oversampling
 oversample = SMOTE()
