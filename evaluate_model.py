ec47124cc8de5e12ff306584daa9bd5a307ea515
Adam Chen <adamchen2301@gmail.com>
2023-07-31 02:29:11 +1000

    Added f1 score metric

--- evaluate_model.py
+++ evaluate_model.py
@@ -1,17 +1,19 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn import metrics
+from sklearn.metrics import confusion_matrix, f1_score
 from sklearn.model_selection import GridSearchCV
 
 def EvaluateModel(X_train, y_train, X_test, y_test, model, grid):
     cv = GridSearchCV(estimator=model, param_grid=grid, cv= 5)
     cv.fit(X_train, y_train.ravel())
     score = cv.best_estimator_.score(X_test, y_test.ravel())
-    print(cv.best_params_)
-    print(cv.best_score_)
-    print(score)
+    print(f"Best parameters: {cv.best_params_}")
+    print(f"best score on training set: {cv.best_score_}")
+    print(f"best model accuracy on test set: {score}")
+    print(f"f1 score: {f1_score(cv.best_estimator_.predict(X_test), y_test.ravel())}")
+
     plt.figure(figsize=(9,9))
-    cm = metrics.confusion_matrix(y_test, cv.best_estimator_.predict(X_test))
+    cm = confusion_matrix(y_test, cv.best_estimator_.predict(X_test))
     sns.heatmap(cm, annot=True, fmt=".3f", linewidths=.5, square = True, cmap = 'Blues_r')
     plt.ylabel('Actual label')
     plt.xlabel('Predicted label')
