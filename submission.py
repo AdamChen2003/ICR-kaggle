ec36cbb6d513ec4c60511ae31e97d32bfeeaed1e
Adam Chen <adamchen2301@gmail.com>
2023-08-03 10:13:48 +1000

    Created file used for kaggle submission

---
+++ submission.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+from imblearn.over_sampling import SMOTE
+
+def PRE(val, data):
+    t = len(data[(data['EJ'] == val) & (data['Class'] == 1)])
+    f = len(data[(data['EJ'] == val) & (data['Class'] == 0)])
+    if f:
+        return t/f
+    return 1
+
+def getBinaryClassData():
+    data = pd.read_csv('datasets/train.csv')
+    for i in data.columns[data.isnull().any(axis=0)]:
+        data[i].fillna(data[i].mean(),inplace=True)
+    X = data.iloc[:,1:57]
+    y = data.iloc[:,57]
+    PRE_A = PRE('A', data)
+    PRE_B = PRE('B', data)
+    X = X.replace('A', PRE_A)
+    X = X.replace('B', PRE_B)
+    
+    return np.array(X), np.array(y).reshape(-1,1), PRE_A, PRE_B
+
+X_train, y_train, PRE_A, PRE_B= getBinaryClassData()
+X_train = StandardScaler().fit_transform(X_train)
+model = AdaBoostClassifier(estimator=DecisionTreeClassifier(criterion='entropy', max_depth=3), n_estimators=500)
+sm = SMOTE(random_state=42)
+X_train, y_train = sm.fit_resample(X_train, y_train)
+clf = model.fit(X_train, y_train.ravel())
+data = pd.read_csv('datasets/test.csv')
+
+for i in data.columns[data.isnull().any(axis=0)]:
+    data[i].fillna(data[i].mean(),inplace=True)
+X = data.iloc[:,1:57]
+X = X.replace('A', PRE_A)
+X = X.replace('B', PRE_B)
+X = StandardScaler().fit_transform(X)
+print(clf.predict_proba(X))
